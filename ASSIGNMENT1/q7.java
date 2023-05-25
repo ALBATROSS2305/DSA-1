c56c53a30349c4ab33ed4df94229634f982afa4e
ALBATROSS2305 <132919588+ALBATROSS2305@users.noreply.github.com>
2023-05-25 11:43:21 +0530

    Update q7.java

--- ASSIGNMENT1/q7.java
+++ ASSIGNMENT1/q7.java
@@ -2,16 +2,16 @@ import java.util.*;
 public class q7 {
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-            // Get the size of the array
+            
             System.out.print("Enter the size of the array: ");
             int n = sc.nextInt();
-            // Declare and initialize the array
+            
             int[] arr = new int[n];
             System.out.print("Enter the elements of the array: ");
             for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
             }
-            // Find the maximum and minimum values
+            
             int max = arr[0];
             int min = arr[0];
             int maxCount = 1;
@@ -34,7 +34,7 @@ public class q7 {
                     minCount++;
                 }
             }
-            // Display the results
+            
             System.out.println("Maximum value: " + max);
             System.out.println("Minimum value: " + min);
             System.out.println("Number of times maximum occurs: " + maxCount);
